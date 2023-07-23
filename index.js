e3609bc2b5399663d3ce3889b28ca6a913e80cdd
hasna <manelhasnahaddoud@gmail.com>
2023-07-23 02:04:21 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const array = ["Charlie", "Samip", "Ali"];
+//const birthday = "birthday";
+function writeCards(array){
+    let messages =[];
+
+for (let i = 0; i < array.length ; i++) 
+   {
+    const name = array[i];
+    messages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+
+   }
+   return messages;
+
+}
+console.log(writeCards());
+function countDown(n){
+    while (n >= 0){
+        console.log(n);
+        --n;
+    }
+}
